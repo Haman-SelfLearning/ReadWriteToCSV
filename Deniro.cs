784357a5342adbcdec343c791bc614e0fdd35d22
Haman shareghi <haman.shareghi@outlook.com>
2022-10-28 12:25:46 +0400

    ADD Class Deniro and Install LinqToCsv

---
+++ Deniro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQtoCSV;
+
+namespace ReadWriteToCSV
+{
+    [Serializable]
+    public class Deniro
+    {
+        [CsvColumn(Name="Year")]
+        public string Year { get; set; }
+        [CsvColumn(Name="Score")]
+        public string Score { get; set; }
+        [CsvColumn(Name = "Title")]
+        public string Title { get; set; }
+
+    }
+}
